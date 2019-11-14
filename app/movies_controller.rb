bc86fdda9205357a7525f25ec419157664840c12
Riley Moore <rilemoore@gmail.com>
2019-11-14 01:10:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,17 +29,17 @@ def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = title
+    m.title = title.title
     m.release_date = release_date
   end
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  Movie.last
 end
 
 def can_get_size_of_the_database
